ab29f215d5fa593f69e4689675b962d2110fdf65
Kenette John A. Antonio <114849172+antonioken22@users.noreply.github.com>
2023-07-30 19:18:10 +0800

    ⬇️ Moved 1/5 of screen height

--- app/(dashboard)/(routes)/dashboard/components/EventModal.js
+++ app/(dashboard)/(routes)/dashboard/components/EventModal.js
@@ -81,7 +81,7 @@ export default function EventModal() {
   }
 
   return (
-    <div className="fixed left-0 top-10 flex justify-center items-center w-full">
+    <div className="fixed left-0 top-20 flex justify-center items-center w-full">
       <form className="bg-white rounded-lg shadow-2xl w-96 max-h-screen">
         <header className="bg-maroon-100 px-4 py-2 flex justify-between items-center">
           <span className="text-maroon-400">
