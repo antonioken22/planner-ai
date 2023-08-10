5c1c50ab4a9f7d8b011011b59dede70643815794
Kenette John A. Antonio <antonio.kenjohn15@gmail.com>
2023-08-10 20:00:37 +0800

    ✨ Added a tooltip instruction

--- app/(dashboard)/(routes)/dashboard/components/top-bar.tsx
+++ app/(dashboard)/(routes)/dashboard/components/top-bar.tsx
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 import CreateEventButton from "./create-event-button";
 import SmallCalendar from "./small-calendar";
 import EventLabels from "./event-labels";
 
 export default function Topbar() {
+  const [openTooltip, setOpenTooltip] = useState(false);
+
+  useEffect(() => {
+    setOpenTooltip(true);
+    const timer = setTimeout(() => setOpenTooltip(false), 3000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <aside className="w-full flex flex-row justify-normal">
       <div className="p-2 border">
-        <CreateEventButton />
+        <TooltipProvider>
+          <Tooltip open={openTooltip} onOpenChange={setOpenTooltip}>
+            <TooltipTrigger>
+              <CreateEventButton />
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>
+                Try clicking the mic icon below and say, "Set an event today."
+              </p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
         <SmallCalendar />
       </div>
       <div className="ml-2 p-2">
